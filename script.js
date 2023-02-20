dc0f8358c17d981ee48433156704fa7e3964e819
Kevin Harp <kharp1984@gmail.com>
2023-02-20 17:41:49 -0500

    js math update

--- script.js
+++ script.js
@@ -4,6 +4,32 @@ let cupImg = document.getElementById('cup');
 let adderOne = document.getElementById('adderOne');
 let adderTwo = document.getElementById('adderTwo');
 let adderThree = document.getElementById('adderThree');
+let chemexQuantity = document.querySelector('#quantityOne');
+let chemexCoffeePerSecond = document.querySelector('#coffeePerSecOne');
+
+let producers = [
+  {
+    name: "Chemex",
+    quantity: 1,
+    coffeePerSecond: 1,
+    cost: "10 Coffee"
+  },
+  {
+    name: "French Press",
+    quantity: 1,
+    coffeePerSecond: 2,
+    cost: "50 Coffee"
+  },
+  {
+    name: "Mr. Coffee",
+    quantity: 1,
+    coffeePerSecond: 5,
+    cost: "100 Coffee"
+  }
+];
+
+chemexQuantity.innerHTML = producers[0].quantity;
+chemexCoffeePerSecond.innerHTML = producers[0].coffeePerSecond;
 
 let coffeeCount = 0;
 cupImg.addEventListener('click', (event) => {
@@ -18,4 +44,5 @@ cupImg.addEventListener('click', (event) => {
     if (coffeeCount > 50) {
       adderThree.style.visibility = "visible";
     }; 
+    return coffeeCount;
 });
