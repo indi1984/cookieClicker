1a5f22b1b2dda0b3d2aea47dc5d1e5103970037e
Kevin Harp <kharp1984@gmail.com>
2023-02-20 17:10:11 -0500

    start of js

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-let people = {
-  name: "kevin",
-  age: 38
-}
+let currentCoffeeCount = document.getElementById('currentCoffeeCount');
+let ttlCoffeeSec = document.getElementById('ttlCoffeeSec');
+let cupImg = document.getElementById('cup');
+let adderOne = document.getElementById('adderOne');
+let adderTwo = document.getElementById('adderTwo');
+let adderThree = document.getElementById('adderThree');
 
-for (obj in people) {
-  console.log(people[obj]);
-};
-
-const keyArray = Object.keys(people);
-for (i of keyArray) {
-  console.log(i);
-};
+let coffeeCount = 0;
+cupImg.addEventListener('click', (event) => {
+  coffeeCount += 1;
+  currentCoffeeCount.innerHTML = coffeeCount;
+    if (coffeeCount > 5) {
+      adderOne.style.visibility = "visible";
+    };
+    if (coffeeCount > 25) {
+      adderTwo.style.visibility = "visible";
+    };
+    if (coffeeCount > 50) {
+      adderThree.style.visibility = "visible";
+    }; 
+});
