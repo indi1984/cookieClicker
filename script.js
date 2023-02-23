f85c09f1ced00c7be31df94c671425b6694ad7a0
Kevin Harp <kharp1984@gmail.com>
2023-02-22 19:12:08 -0500

    format fixes

--- script.js
+++ script.js
@@ -15,38 +15,36 @@ let btnOne = document.getElementById('btnOne');
 let btnTwo = document.getElementById('btnTwo');
 let btnThree = document.getElementById('btnThree');
 
-
 // * Initialize Array
 let producers = [
   {
-    name: "Chemex",
+    name: 'Chemex',
     quantity: 0,
     coffeePerSecond: 1,
     ttlCoffeePerSecond: function () {
       return this.quantity * this.coffeePerSecond;
     },
-    cost: 10
+    cost: 10,
   },
   {
-    name: "French Press",
+    name: 'French Press',
     quantity: 0,
     coffeePerSecond: 2,
     ttlCoffeePerSecond: function () {
       return this.quantity * this.coffeePerSecond;
     },
-    cost: 50
+    cost: 50,
   },
   {
-    name: "Mr. Coffee",
+    name: 'Mr. Coffee',
     quantity: 0,
     coffeePerSecond: 3,
     ttlCoffeePerSecond: function () {
       return this.quantity * this.coffeePerSecond;
     },
-    cost: 100
-  }
+    cost: 100,
+  },
 ];
-
 
 // * Set initial default counters on HTML
 chemexQuantity.innerHTML = producers[0].quantity;
@@ -58,87 +56,83 @@ frenchPressCoffeePerSecond.innerHTML = producers[1].coffeePerSecond;
 mrCoffeeQuantity.innerHTML = producers[2].quantity;
 mrCoffeeCoffeePerSecond.innerHTML = producers[2].coffeePerSecond;
 
-
 // * Show producers as enough coffee is clicked
 let coffeeCount = 0;
 cupImg.addEventListener('click', (event) => {
   coffeeCount += 1;
   showProducers();
 });
-function showProducers () {
+function showProducers() {
   currentCoffeeCount.innerHTML = coffeeCount;
-    if (coffeeCount >= 5) {
-      adderOne.style.visibility = "visible";
-    };
-    if (coffeeCount >= 25) {
-      adderTwo.style.visibility = "visible";
-    };
-    if (coffeeCount >= 50) {
-      adderThree.style.visibility = "visible";
-    }; 
-    return coffeeCount;
-};
-
+  if (coffeeCount >= 5) {
+    adderOne.style.visibility = 'visible';
+  }
+  if (coffeeCount >= 25) {
+    adderTwo.style.visibility = 'visible';
+  }
+  if (coffeeCount >= 50) {
+    adderThree.style.visibility = 'visible';
+  }
+  return coffeeCount;
+}
 
 // * Producer Button Click Events
 btnOne.addEventListener('click', chooseProducerOne);
 btnTwo.addEventListener('click', chooseProducerTwo);
 btnThree.addEventListener('click', chooseProducerThree);
 
-
-/// * Button Click Event Handler Functions
-function chooseProducerOne () {
+// * Button Click Event Handler Functions
+function chooseProducerOne() {
   if (coffeeCount >= producers[0].cost) {
     producers[0].quantity += 1;
     updateCoffeePerSec();
-     chemexQuantity.innerHTML = producers[0].quantity;
-      if (coffeeCount > 0) {
-        coffeeCount = coffeeCount - producers[0].cost;
-      };
+    chemexQuantity.innerHTML = producers[0].quantity;
+    if (coffeeCount > 0) {
+      coffeeCount = coffeeCount - producers[0].cost;
+    }
   } else {
     alert('NOT ENOUGH COFFEE!');
-  };
+  }
   currentCoffeeCount.innerHTML = coffeeCount;
-};
-function chooseProducerTwo () {
+}
+function chooseProducerTwo() {
   if (coffeeCount >= producers[1].cost) {
     producers[1].quantity += 1;
     updateCoffeePerSec();
     frenchPressQuantity.innerHTML = producers[1].quantity;
-      if (coffeeCount > 0) {
-        coffeeCount = coffeeCount - producers[1].cost;  
-      };
+    if (coffeeCount > 0) {
+      coffeeCount = coffeeCount - producers[1].cost;
+    }
   } else {
     alert('NOT ENOUGH COFFEE!');
-  };
+  }
   currentCoffeeCount.innerHTML = coffeeCount;
-};
-function chooseProducerThree () {
+}
+function chooseProducerThree() {
   if (coffeeCount >= producers[2].cost) {
     producers[2].quantity += 1;
     updateCoffeePerSec();
     mrCoffeeQuantity.innerHTML = producers[2].quantity;
-      if (coffeeCount > 0) {
-        coffeeCount = coffeeCount - producers[2].cost;
-      };
+    if (coffeeCount > 0) {
+      coffeeCount = coffeeCount - producers[2].cost;
+    }
   } else {
     alert('NOT ENOUGH COFFEE!');
-  };
+  }
   currentCoffeeCount.innerHTML = coffeeCount;
-};
+}
 
-
-// * Loop though to get total current Coffee per Second   
-function updateCoffeePerSec () {
+// * Loop though to get total current Coffee per Second
+function updateCoffeePerSec() {
   let newCoffeeSecSum = 0;
-    for (let i = 0; i < producers.length; i++) {
-      newCoffeeSecSum += producers[i].ttlCoffeePerSecond();
-    };
+  for (let i = 0; i < producers.length; i++) {
+    newCoffeeSecSum += producers[i].ttlCoffeePerSecond();
+  }
   setInterval(function () {
     coffeeCount += newCoffeeSecSum;
     // currentCoffeeCount.innerHTML = coffeeCount;
     showProducers();
-  }, 1000);   
+  }, 1000);
   ttlCoffeeSec.innerHTML = newCoffeeSecSum;
   return newCoffeeSecSum;
-};
+}
